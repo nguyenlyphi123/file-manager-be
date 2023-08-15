8f6606123bf79239a2d267ec857fb53b1ded2111
nguyenlyphi123 <nguyenlyphi247@gmail.com>
2023-08-15 13:41:55 +0700

    fix: gg auth

--- config/googleStorage.js
+++ config/googleStorage.js
@@ -1,7 +1,9 @@
 const { Storage } = require('@google-cloud/storage');
 
 const projectId = process.env.GCLOUD_PROJECT_ID;
-const privateKey = process.env.GCLOUD_PRIVATE_KEY;
+const privateKey = process.env.GCLOUD_PRIVATE_KEY.split(String.raw`\n`).join(
+  '\n',
+);
 const privateKeyId = process.env.GCLOUD_PRIVATE_KEY_ID;
 const clientEmail = process.env.GCLOUD_CLIENT_EMAIL;
 const clientId = process.env.GCLOUD_CLIENT_ID;
@@ -13,7 +15,7 @@ const storage = new Storage({
     type: 'service_account',
     project_id: projectId,
     private_key_id: privateKeyId,
-    private_key: privateKey.replace(/\\n/g, '\n'),
+    private_key: privateKey,
     client_email: clientEmail,
     client_id: clientId,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
